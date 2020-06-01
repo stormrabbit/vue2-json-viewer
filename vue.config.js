223b7556857a03870a9ed03064874c8575863d35
龙骑将杨影枫 <catwindboy@hotmail.com>
2020-06-01 11:26:11 +0800

    reset to init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
+const config = require('./package.json');
+console.log(`使用服务端域名 ==> ${process.env.VUE_APP_API_HOST}`);
+console.log(`当前版本 ==> ${config.version}`);
+
+const Timestamp = `${new Date().getTime()}`;
 module.exports = {
+  configureWebpack: { // webpack 配置
+    output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
+      filename: `[name].${Timestamp}.${config.version}.js`,
+      chunkFilename: `[name].${Timestamp}.${config.version}.js`
+    },
+  },
+  productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
   "transpileDependencies": [
     "vuetify"
   ],
